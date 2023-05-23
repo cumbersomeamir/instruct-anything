ba7851a49476775a8072705302ab4733037b1954
Amir Kidwai <40836125+cumbersomeamir@users.noreply.github.com>
2023-05-23 22:26:32 +0100

    Update train.py

--- train.py
+++ train.py
@@ -202,11 +202,11 @@ def train():
     trainer = Trainer(model=model, tokenizer=tokenizer, args=training_args, **data_module)
     trainer.train()
     trainer.save_state()
-    trainer.save_model(output_dir=training_args.output_dir)
+    #trainer.save_model(output_dir=training_args.output_dir)
     # Save the fine-tuned model
-    trainer.save_model("finetuned_MedQuad”)
+    trainer.save_model('finetuned_MedQuad')
 
-    model = AutoModelForCausalLM.from_pretrained("finetuned_MedQuad')
+    model = AutoModelForCausalLM.from_pretrained('finetuned_MedQuad')
 
     #Saving the Model on huggingface
     token = "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"
